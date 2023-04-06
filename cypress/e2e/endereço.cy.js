2f8c77a9469d5868903f779caef3bf34f6aa0474
Kauan Butura <marciobutura@outlook.com>
2023-04-06 18:17:56 -0300

    Adicionando novas testes no arquivo endereço.cy.js

--- cypress/e2e/endereço.cy.js
+++ cypress/e2e/endereço.cy.js
@@ -1,13 +1,16 @@
 /// <reference types='cypress' />
+import endereçoPage from "../support/page-objects/endereço-page";
 
-beforeEach(() => {
-  cy.visit('minha-conta/')
-  cy.login('josegabriel@gmail.com', '12345')
-});
+describe('Funcionalidade Faturamento e Entrega', () => {
+  beforeEach(() => {
+    cy.visit('minha-conta/')
+    cy.login('josegabriel@gmail.com', '12345')
+  });
 
-describe('Funcionalidade de Endereços - Faturamento de entrega', () => {
+  it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    endereçoPage.editarEndereçoFaturamento("Kauan", "Butura", "Ebac", "Inajar de Souza", "apartamento",
+      "São Paulo", "São Paulo", "02721-200", "2728474837", "josemario@gmail.com")
 
-  it('', () => {
-
+    cy.get('.woocommerce-message').should('contain', "Endereço alterado com sucesso.")
   });
 });
